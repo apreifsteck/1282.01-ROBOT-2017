7942846eb923c54c0f0bf7b175ed8c3e92cc9a16
Garrett Haufschild <ghaufschild@garretts-macbook-pro.local>
2017-02-24 15:21:57 -0500

    Initial Commit

---
+++ Stuff/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stuff
+//
+//  Created by Garrett Haufschild on 2/24/17.
+//  Copyright © 2017 Swag Productions. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

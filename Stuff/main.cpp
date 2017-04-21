52e807c222af15ff4f33826d278ab1ebc79c362a
Garrett Haufschild <ghaufschild@garretts-macbook-pro.local>
2017-04-21 17:44:45 -0400

    Final Project and Competition Ready Code

--- Stuff/main.cpp
+++ Stuff/main.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Swag Productions. All rights reserved.
 //
 
+//Included Libraries
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
@@ -18,20 +19,20 @@
 #include <time.h>
 
 
-//GLOBALS
-//Starting location: X) 5.4 Y) 26.5
-/*
+/*  Average location values
  Start Location:         5.699	26.400
  Drop Light Location:    11.599	15.199
  Lever Location:         12.400	46.599
  Button Location:        25.300	62.800
  */
+
+//GLOBALS
 float LIGHTX = 11.4;
 float LIGHTY = 15.4;
 float LEVERX = 11.4;
 float LEVERY = 46.9;
-float BUTTONX = 25;
-float BUTTONY = 63.199;
+float BUTTONX = 25.9;
+float BUTTONY = 62.6;
 float COREX = 18.0;
 float COREY = 54.3;
 
@@ -41,8 +42,8 @@ FEHMotor fRight(FEHMotor::Motor1, 12.0);
 FEHMotor bLeft(FEHMotor::Motor2, 12.0);
 FEHMotor bRight(FEHMotor::Motor3, 12.0);
 FEHServo serv(FEHServo::Servo0);
-//Sensors
 
+//Sensors
 //bump setup
 //   0        1
 //7              2
@@ -81,43 +82,7 @@ const double M_PI = 3.14159265358979323846264338327950288;
 const int servMin = 990;
 const int servMax = 1893;
 
-const float CORE_LINE_THRESHOLD = 0;
-const float DUMP_LINE_THRESHOLD = 0;
-
-
-//tasks
-#define TURN_SAT 0
-#define READ_LIGHT 1
-#define PUSH_BTN 2
-#define PULL_LEVER 3
-#define EXTRACT_CORE 4
-#define DUMP_CORE 5
-#define PUSH_END_BTN 6
-
-#define RED_LIGHT 1
-
 //Movement Methods
-
-
-
-
-struct Task{
-    float startX;
-    float startY;
-    int direction;
-    bool completed;
-    float timeout;
-};
-
-struct State{
-    float currentX;
-    float currentY;
-    int currentDirection;
-    float time;
-};
-
-
-
 double DegreeToRadian(int degree) {
     return (degree / 180.0) * M_PI;
     
@@ -139,53 +104,6 @@ void AllMotors(int power)
 }
 
 /*
- * USE WHEN RPS IS PRESENT
- * Uses current information to find where to move
- */
-void Drive(int power, float x, float y) {
-    float direction = RPS.Heading();
-    while((int)direction % 45 > 2) {
-        AllMotors(8);
-    }
-    
-    float curX = RPS.X();
-    float curY = RPS.Y();
-    
-    float deltaX = (x - curX);
-    float deltaY = (y - curY);
-    
-    float arctan = (deltaY/deltaX) + 180 + M_PI/4.0;
-    float degree = direction - arctan;
-    
-    
-    deltaX = cos(degree);
-    deltaY = sin(degree);
-    
-    double xPow = deltaX * power;
-    double yPow = deltaY * power;
-    
-    StopMotors();
-    
-    fLeft.SetPercent(xPow);
-    bRight.SetPercent(-xPow);
-    fRight.SetPercent(-yPow);
-    bLeft.SetPercent(yPow);
-    LCD.Clear();
-    while(abs(RPS.X() - x) > 1 || abs(RPS.Y() - y) > 1) {
-        SD.Printf("RPS Heading: \t%f\t%f\n", RPS.Heading(), degree);
-        SD.Printf("X: %f     Y: %f\n", RPS.X(), RPS.Y());
-        SD.Printf("Goal X: %f     Goal Y: %f\n\n", x, y);
-        
-        LCD.WriteRC(RPS.X(),1,1);
-        LCD.WriteRC(RPS.Y(),1,7);
-        LCD.WriteRC(abs(RPS.X() - x),3,1);
-        LCD.WriteRC(abs(RPS.Y() - y),3,7);
-    }
-    StopMotors();
-}
-
-/*
- *  FOR USE WHEN NO RPS IS PRESENT
  *  Takes the direction the robot is facing and the direction it wishes to travel
  *  and uses the unit circle to find the power to apply to each motor.
  */
@@ -204,6 +122,10 @@ void Drive(int power, int moveDirection, int facingDirection) {
     bLeft.SetPercent(yPow);
 }
 
+/*
+ * Used to rotate the robot to whatever direction it needs to face,
+ * rotation speed is directly proportional to the angle left to turn.
+ */
 void Rotate(int degree) {
     LCD.WriteLine(RPS.Heading());
     StopMotors();
@@ -229,31 +151,9 @@ void Rotate(int degree) {
     StopMotors();
 }
 
-void RotateSlow(int degree) {
-    LCD.WriteLine(RPS.Heading());
-    StopMotors();
-    int modifier = 1;
-    if((int)(RPS.Heading() - degree + 360) % 360 < 180) {
-        modifier *= 1;
-    }
-    else {
-        modifier *= -1;
-    }
-    int degRem = (int)(RPS.Heading() - degree + 360) % 360;
-    if(degRem >= 180) {
-        degRem = 360-degRem;
-    }
-    while(degRem > 3) {
-        degRem = (int)(RPS.Heading() - degree + 360) % 360;
-        if(degRem >= 180) {
-            degRem = 360-degRem;
-        }
-        int power = (degRem / 7 + 7) * modifier;
-        AllMotors(power);
-    }
-    StopMotors();
-}
-
+/*
+ * Used to find if the robot is on the line.
+ */
 bool DetectLine(AnalogInputPin sensor, float threshold) {
     if (sensor.Value() < threshold) {
         return true;
@@ -261,6 +161,10 @@ bool DetectLine(AnalogInputPin sensor, float threshold) {
     return false;
 }
 
+/*
+ * The robot will drive on a line found using the given threshold
+ * towrads the desired direction for however long is given.
+ */
 float DriveOnLine(int motorPercent, int moveDirection, int facingDirection, float threshold, double elapsedTime) {
     LCD.Clear();
     double time = TimeNow();
@@ -276,37 +180,38 @@ float DriveOnLine(int motorPercent, int moveDirection, int facingDirection, floa
             bRight.SetPercent(-motorPercent);
             LCD.WriteRC("On Line", 7 , 1);
         } else if((DetectLine(rLine, threshold) && DetectLine(cLine, threshold)) || DetectLine(rLine,threshold)) {   //to the left of the line
-            //Drive(10, moveDirection - 45, facingDirection);
             fLeft.SetPercent(0);
             fRight.SetPercent(motorPercent);
             bLeft.SetPercent(-motorPercent);
             bRight.SetPercent(0);
             LCD.WriteRC("Left of line", 7 , 1);
         } else if((DetectLine(lLine, threshold) && DetectLine(cLine, threshold)) || DetectLine(lLine,threshold)) {  //to the right of the line
-            //Drive(10, moveDirection + 45, facingDirection);
             fLeft.SetPercent(motorPercent);
             fRight.SetPercent(0);
             bLeft.SetPercent(0);
             bRight.SetPercent(-motorPercent);
             LCD.WriteRC("Right of line", 7 , 1);
         }
-        else {
+        else {  //Off line completey
             fLeft.SetPercent(0);
             fRight.SetPercent(motorPercent);
             bLeft.SetPercent(-motorPercent);
             bRight.SetPercent(0);
-            LCD.WriteRC("Shit is suppppperrr fucked.", 7, 1);
+            LCD.WriteRC("Shit is super messed up.", 7, 1);
             StopMotors();
         }
     }
 }
 
+/*
+ * Used to grab RPS values of the bin light and lever values to account
+ * for different RPS values from course to course.
+ */
 void CalibrateRPS(){
     LCD.Clear( FEHLCD::Black );
     LCD.SetFontColor( FEHLCD::White );
     
     RPS.InitializeTouchMenu();
-    SD.OpenLog();
     
     LCD.Clear( FEHLCD::Black );
     serv.SetMin(servMin);
@@ -322,22 +227,24 @@ void CalibrateRPS(){
     LEVERX = RPS.X();
     LEVERY = RPS.Y();
     Sleep(1000);
-    //    while(switch7.Value()) {LCD.WriteRC(RPS.X(),0,0);LCD.WriteRC(RPS.Y(),1,0);}
-    //    COREX = RPS.X();
-    //    COREY = RPS.Y();
-    //    LCD.WriteLine("Core Values Found.");
     while(switch6.Value()) {}
     LCD.WriteLine("Getting Ready.");
     serv.SetDegree(180);
     Sleep(2000);
 }
 
-
-
+/*
+ * Waits until a light is detected or 30 seconds pass.
+ */
 bool StartLightOn() {
-    while(cds.Value() > 1){    LCD.WriteLine(cds.Value());  }
+    double time = TimeNow();
+    while(cds.Value() > 1 && TimeNow() - time < 30){}
 }
 
+/*
+ * Robot moves from the start location to the core light, moves northwest
+ * and then north.
+ */
 void moveToCoreLight() {
     Drive(30, NORTHWEST, NORTH);
     while(RPS.X() < LIGHTX - 1.5) {}
@@ -350,6 +257,9 @@ void moveToCoreLight() {
     StopMotors();
 }
 
+/*
+ * Readjust location until it is with half an inch of the light.
+ */
 void fixCoreLight() {
     Drive(10, WEST, NORTH);
     while(RPS.X() < LIGHTX - 0.5) {}
@@ -362,27 +272,39 @@ void fixCoreLight() {
     StopMotors();
 }
 
+/*
+ * Attempts to read core light value, if no acceptable values are found
+ * then the robot readjusts and tries again. If it still doesn't work the
+ * robot assumes red and continues.
+ */
 int readCoreLight() {
     int value = 0;
-    while(value == 0) {
-        if(cds.Value() < .55) {                  //RED
+    int i = 0;
+    while(value == 0 && i < 2) {
+        if(cds.Value() < .75) {                  //RED
             LCD.WriteLine("Light: RED");
             LCD.Clear( FEHLCD::Red );
             value = 1; //Returns one for on red light
-        } else if(cds.Value() < 1){             //BLUE
+        } else if(cds.Value() < 1.7){           //BLUE
             LCD.WriteLine("Light: BLUE");
             LCD.Clear( FEHLCD::Blue );
-            value = -1; //returns 0 for on blue light
-        } else {
+            value = -1; //returns -1 for on blue light
+        } else {                                //for on no light
             LCD.WriteLine("Not on light.");
             LCD.Clear( FEHLCD::Green );
+            LCD.WriteLine(cds.Value());
             fixCoreLight();
+            i++;
         }
     }
-    return value; //for on no light
+    return value;
 }
 
-//Works on the assumption that you start from the switch
+/*
+ * Moves southwest from the lever to the button quickly. Once in line with
+ * the button, drive south for 5.5 seconds to ensure button is pressed for 5
+ * full seconds
+ */
 void PushButton() {
     Rotate(NORTH);
     Drive(100, SOUTHWEST, NORTH);
@@ -394,6 +316,12 @@ void PushButton() {
     Sleep(5500);
 }
 
+/*
+ * Moves the robot from the bottom of the ramp up and then lines up the robot
+ * with the Y coordinate of the lever and then drives east until it reaches the
+ * lever. It then readjusts its Y coordinate one more time before lowering the
+ *  lever arm and backing up and then raising the lever arm.
+ */
 void PullLever() {
     Rotate(EAST);
     Drive(70, EAST, EAST);
@@ -403,15 +331,13 @@ void PullLever() {
     StopMotors();
     Sleep(300);
     Drive(40, NORTH, EAST);
-    while(RPS.Y() < 0 || RPS.Y() > 47.5) {
+    while(RPS.Y() < 0 || RPS.Y() > 48.0) {
         LCD.Clear( FEHLCD::Green );
         LCD.WriteRC(RPS.X(),1,1);
         LCD.WriteRC(RPS.Y(),2,1);
     }
-    SD.Printf("%f, %f\n", RPS.X(), RPS.Y());
     StopMotors();
     Rotate(NORTH);
-    SD.Printf("%f, %f\n", RPS.X(), RPS.Y());
     Drive(40, EAST, NORTH);
     while(RPS.X() > LEVERX + 1.75) {}
     Drive(20, NORTH, NORTH);
@@ -424,6 +350,12 @@ void PullLever() {
     serv.SetDegree(180);
 }
 
+/*
+ * Robot drives southwest and the west until it reaches the west wall,
+ * it then drives north until it runs into the satellite and then drives
+ * east until it reaches the X coordinate 25, it then drives southeast off
+ * the satellite towards the bottom of the ramp.
+ */
 void TurnSatellite() {
     Drive(50, SOUTHWEST, NORTH);
     Sleep(200);
@@ -440,8 +372,11 @@ void TurnSatellite() {
     StopMotors();
 }
 
+/*
+ * The drives east or southeast depending on which bin it was at.
+ * The robot then backs up until the stop button is press.
+ */
 void PushEndButton(int lightType) {
-    SD.CloseLog();
     if(lightType == 1) {
         Drive(50, SOUTHEAST, WEST);
         Sleep(300);
@@ -453,10 +388,11 @@ void PushEndButton(int lightType) {
     Drive(50, SOUTH, WEST);
 }
 
-void ResetTask(struct Task){
-    
-}
-
+/*
+ * This method drives the robot back until it reaches the YCoord of
+ * the core, then east untli the XCoord is reached, correcting itself
+ * it then rotates northeast
+ */
 void SetupCore() {
     Drive(50, NORTH, NORTH);
     while(RPS.Y() > COREY + 2.5) {}
@@ -473,6 +409,12 @@ void SetupCore() {
     Rotate(NORTHEAST);
 }
 
+/*
+ * This method drives the robot north for .5 seconds then northeast until
+ * either the X or Y coord of the core is reached, if it enters the deadzone
+ * on its path it relies back the SetupCore method before or else it will
+ * rotate northeast
+ */
 void SetupCore2() {
     Drive(20, NORTH, NORTH);
     Sleep(500);
@@ -491,6 +433,11 @@ void SetupCore2() {
     }
 }
 
+/*
+ * This method drives north for .65 seconds before rotating northeast,
+ * the robot will drive east until a line is found and at the same time
+ * lower the lever arm.
+ */
 void SetupCore3() {
     Drive(40, NORTH, NORTH);
     Sleep(650);
@@ -501,6 +448,12 @@ void SetupCore3() {
     
 }
 
+/*
+ * Lowers the lever arm and drives southeast for a second, it then drives
+ * on the line for 2 seconds, it then drives northwest for a second and then
+ * it starts raising the lever arm until it is upright and keeps driving until
+ * it is out of the deadzone.
+ */
 void ExtractCore(){
     serv.SetDegree(0);
     Drive(40, SOUTHEAST, NORTHEAST);
@@ -517,8 +470,13 @@ void ExtractCore(){
     StopMotors();
 }
 
+/*
+ * Drives southeast on the line for 1.15 seconds and then drives backwards
+ * for .5 seconds and then raises the lever arm until it is upright and
+ * keeps driving until it is out of the deadzone.
+ */
 void ExtractCore2() {
-    DriveOnLine(25, SOUTHEAST, NORTHEAST, 1.7, 1.5);
+    DriveOnLine(25, SOUTHEAST, NORTHEAST, 1.7, 1.15);
     Drive(45, NORTHWEST, NORTHEAST);
     Sleep(500);
     for(int i = 0; i < 179; i+=5) {
@@ -529,16 +487,25 @@ void ExtractCore2() {
     StopMotors();
 }
 
+/*
+ * Makes the robot rotate west and drive down the hill until it is close to
+ * the Y coord of the bin light, it then drives east until it is line with
+ * the correct bin. It then drives forward, lowers the arm into the bin. The
+ * robot then spins clockwise for .4 seconds, it then raises and lowers the lever
+ * arm twice and then raises it and it rotates west again and drives south for .5
+ * seconds and stops the motors.
+ */
 void DumpCore(int lightType) {
     Rotate(WEST);
     Drive(70, NORTH, WEST);
-    while(RPS.Y() > LIGHTY + 5.50 || RPS.Y() < 0);
+    double yVal = RPS.Y();
+    while(yVal > LIGHTY + 5.50 || yVal < 0) {yVal = RPS.Y();}
     Rotate(WEST);
-    double goalX = 5;
-    double buffer = 4.2;
+    double goalX = LIGHTX - 6.0;
+    double buffer = 3.7;
     if(lightType == -1) {
-        goalX = 15.0;
-        buffer = 1.0;
+        goalX = LIGHTX + 3.5;
+        buffer = 0.5;
     }
     Drive(10, WEST, WEST);
     while (RPS.X() < goalX + 0.5);
@@ -553,47 +520,62 @@ void DumpCore(int lightType) {
     StopMotors();
     serv.SetDegree(0);
     Sleep(300);
-    serv.SetDegree(90);
-    Sleep(150);
-    serv.SetDegree(0);
-    Sleep(150);
-    serv.SetDegree(90);
-    Sleep(150);
-    serv.SetDegree(0);
-    Sleep(150);
-    serv.SetDegree(90);
-    Sleep(150);
-    serv.SetDegree(0);
-    Sleep(150);
+    AllMotors(30);
+    Sleep(400);
+    StopMotors();
     serv.SetDegree(180);
+    Sleep(300);
+    serv.SetDegree(0);
+    Sleep(300);
+    serv.SetDegree(180);
+    Sleep(300);
+    serv.SetDegree(0);
+    Sleep(300);
+    serv.SetDegree(180);
+    Rotate(WEST);
     Drive(50, SOUTH, WEST);
     Sleep(500);
     StopMotors();
 }
 
-//int main(void) {
-//    RPS.InitializeTouchMenu();
-//    LCD.Clear();
-//    while(true) {
-//    LCD.WriteRC(RPS.X(),0,0);LCD.WriteRC(RPS.Y(),1,0);
-//    }
-//    //LIGHT: 11.4, 15.4
-//    //BINRED: 6.8, 14.6
-//    //BINBLUE: 14.9 14.8
-//    //WALL: 31.5
-//    //RAMP: 18.9
-//    //LEVER: 47.2, 12.0
-//    //CORE: 18.9, 54.7
-//    //END: 6.0
-//}
+/*      Used as testing method for getting needed values of a new environment
+ int main(void) {
+ RPS.InitializeTouchMenu();
+ SD.OpenLog();
+ LCD.Clear();
+ while(true) {
+ LCD.WriteRC(RPS.X(),1,0);
+ LCD.WriteRC(RPS.Y(),2,0);
+ LCD.WriteRC(lLine.Value(),3,0);
+ LCD.WriteRC(cLine.Value(),4,0);
+ LCD.WriteRC(rLine.Value(),5,0);
+ LCD.WriteAt(cds.Value(),6,0);
+ if(!switch7.Value()) {
+ SD.Printf("X: %f\n", RPS.X());
+ SD.Printf("Y: %f\n", RPS.Y());
+ SD.Printf("Left: %f\n", lLine.Value());
+ SD.Printf("Center: %f\n", cLine.Value());
+ SD.Printf("Right: %f\n", rLine.Value());
+ SD.Printf("CDS: %f\n\n", cds.Value());
+ Sleep(500);
+ }
+ }
+ SD.CloseLog();
+ }
+ */
 
-
+/*
+ * Main method the completes all the tasks in the correct order.
+ */
 int main(void) {
     CalibrateRPS();
     int lightType = 1;
     StartLightOn();
     moveToCoreLight();
     lightType = readCoreLight();
+    if(lightType==0) {
+        lightType = 1;
+    }
     TurnSatellite();
     PullLever();
     PushButton();
@@ -601,20 +583,4 @@ int main(void) {
     ExtractCore2();
     DumpCore(lightType);
     PushEndButton(lightType);
-    LCD.WriteLine("Finished");
-    
-    //24 away from core
-    //72 is max y, 36 is max x
-    //put it 18 away from maxes
 }
-
-
-/*
- int main(void) {
- while(true) {
- LCD.WriteRC(lLine.Value(),0,0);
- LCD.WriteRC(cLine.Value(),1,0);
- LCD.WriteRC(rLine.Value(),2,0);
- }
- }
- */
